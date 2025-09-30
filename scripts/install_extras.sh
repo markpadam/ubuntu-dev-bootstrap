eb7852bfa202a108b806c4063b1282cd4f1cd1e4
Mark Adam <37782147+markpadam@users.noreply.github.com>
2025-09-30 20:55:33 +0100

    Add install_extras.sh script for additional development tools

---
+++ scripts/install_extras.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install -y fzf bat ripgrep tmux
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+sudo snap install gh
