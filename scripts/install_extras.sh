1cc98951085097786996846bc60219a06c090743
Mark Adam <37782147+markpadam@users.noreply.github.com>
2025-10-01 14:13:23 +0000

    Refactor install_k9s.sh to include installation of kubectx, kubens, helm, and kubelogin

--- scripts/install_extras.sh
+++ scripts/install_extras.sh
@@ -3,7 +3,3 @@ sudo apt install -y fzf bat ripgrep tmux
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 sudo snap install gh
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-
- sudo snap install helm --classic
--- scripts/install_k9s.sh
+++ scripts/install_k9s.sh
@@ -1,5 +1,12 @@
+# install Kubernetes Tools
 #!/bin/bash
 curl -Lo k9s.tar.gz https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz
 tar -xzf k9s.tar.gz
 sudo mv k9s /usr/local/bin/
 rm k9s.tar.gz
+
+# install other useful tools
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+sudo snap install helm --classic
+curl -LO https://github.com/Azure/kubelogin/releases/latest/download/kubelogin-linux-amd64.zip
