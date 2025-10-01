288682fd9644239ff7ef2ff6719cb12f3f8fbc49
Mark Adam <37782147+markpadam@users.noreply.github.com>
2025-10-01 08:10:23 +0000

    added kubectx

--- scripts/install_extras.sh
+++ scripts/install_extras.sh
@@ -3,3 +3,5 @@ sudo apt install -y fzf bat ripgrep tmux
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 sudo snap install gh
+sudo apt install kubectx
+sudo apt install kubens
