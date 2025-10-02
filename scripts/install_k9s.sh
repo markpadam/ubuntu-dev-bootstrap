0c2ae1e23814a128c4834eda6993aa76eaf398d8
Mark Adam <37782147+markpadam@users.noreply.github.com>
2025-10-02 11:36:21 +0000

    Refactor install_kubectl.sh and install_k9s.sh to include installation of kubectx, kubens, helm, and kubelogin

--- scripts/install_k9s.sh
+++ scripts/install_k9s.sh
@@ -1,12 +1,5 @@
-# install Kubernetes Tools
 #!/bin/bash
 curl -Lo k9s.tar.gz https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz
 tar -xzf k9s.tar.gz
 sudo mv k9s /usr/local/bin/
 rm k9s.tar.gz
-
-# install other useful tools
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-sudo snap install helm --classic
-sudo snap install kubelogin
--- scripts/install_kubectl.sh
+++ scripts/install_kubectl.sh
@@ -2,3 +2,9 @@
 curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
+
+# install other useful tools
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+sudo snap install helm --classic
+sudo snap install kubelogin
